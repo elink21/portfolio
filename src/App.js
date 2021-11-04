e35b00f41bc2c75fb8ebc269388a27f5f67b3335
Elias Segundo Antonio <eliassegundo.segundo@gmail.com>
2021-11-03 21:30:27 -0500

    Revert "[["
    This reverts commit 6e650c4fd22310b37edb546c433d960eddb22202.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import "./App.css";
-import "./Home";
-import Home from "./Home";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <ChakraProvider>
-      <Home></Home>
-    </ChakraProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
